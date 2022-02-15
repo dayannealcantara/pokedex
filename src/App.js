84c28fa742638cb8769c4e971c38de131ead8317
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-15 16:34:14 -0300

    Constrindo Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
