b9ece17a77d3013a6d0ff2185c474e4d21240de1
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-17 10:50:06 -0300

    page: Buscando lista de pokemon

--- src/page/ListPokemon/index.js
+++ src/page/ListPokemon/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PokemonCard from "../../components/PokemonCard";
+import api from '../../services/api';
+
 import {
   Container,
   Wrapper,
@@ -9,25 +12,81 @@ import {
   TextFavorito,
   Buscar,
   ContainerMenu,
+  App,
+  PaginationContainer,
+  SearchContainer,
+  PokemonsContainer,
 } from './styles';
 import Logopesquisar from '../../imagens/pesqui.png';
 import Logofavoritos from '../../imagens/curtir.png';
 
-function ListPokemon() {
+function Home({ getQuery }) {
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [query, setQuery] = useState('');
+  const [text, setText] = useState('');
+
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
+  };
+
+  
+ 
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      setIsLoading(true);
+      const response = await api.get('pokemon?limit=15&offset=0');
+      const { data } = response;
+     
+      setPokemons(data.results)
+      setIsLoading(false);
+    };
+    fetchPokemons();
+  }, []);
+
+  console.log(pokemons)
+
+  const renderPokemonsList = () => {
+    const pokemonsList = [];
+
+    pokemons.forEach((pokemon) => {
+      if (!pokemon.name.includes(query)) {
+        return;
+      }
+
+      pokemonsList.push(<PokemonCard key={pokemon.name} pokemon={pokemon} />);
+    });
+
+    return pokemonsList;
+  };
+
+
   return (
     <Container>
       <Wrapper>
-        <Fieldset>
-          <Legend>Buscar</Legend>
-          <Buscar placeholder={'Busque seu Pokemon'} />
-          <LogoBuscar src={Logopesquisar} alt="logo de pesquisa" />
-        </Fieldset>
-        <ContainerMenu>
-          <LogoFavoritos src={Logofavoritos} alt="logo favoritos" />
-          <TextFavorito>Meus Favoritos</TextFavorito>
-        </ContainerMenu>
+        <SearchContainer>
+          <Fieldset>
+            <Legend>Buscar</Legend>
+            <Buscar
+              type="text"
+              placeholder={'Busque seu Pokemon'}
+              value={text}
+              onChange={(e) => onChange(e.target.value)}
+            />
+            <LogoBuscar src={Logopesquisar} alt="logo de pesquisa" />
+          </Fieldset>
+          <ContainerMenu>
+            <LogoFavoritos src={Logofavoritos} alt="logo favoritos" />
+            <TextFavorito>Meus Favoritos</TextFavorito>
+          </ContainerMenu>
+        </SearchContainer>
+        <PokemonsContainer>
+        {renderPokemonsList()}
+        </PokemonsContainer>
       </Wrapper>
     </Container>
   );
 }
-export default ListPokemon;
+export default Home;
