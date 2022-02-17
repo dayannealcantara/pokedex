3b3629534eaa1dd1110c28b68507a5715c19c922
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-17 10:50:26 -0300

    Page home principal

---
+++ src/page/Home/index.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import PokemonCard from '../../components/PokemonCard';
+import api from '../../services/api';
+
+import {
+  Container,
+  Wrapper,
+  Fieldset,
+  Legend,
+  LogoBuscar,
+  LogoFavoritos,
+  TextFavorito,
+  Buscar,
+  ContainerMenu,
+  SearchContainer,
+  PokemonsContainer,
+} from './styles';
+import Logopesquisar from '../../imagens/pesqui.png';
+import Logofavoritos from '../../imagens/curtir.png';
+
+function ListPokemon({ getQuery }) {
+  const NUMBER_POKEMONS = 15;
+  const NUMBER_MAX_POKEMONS_API = 750;
+
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemonSearch, setPokemonSearch] = useState('');
+  const [pokemonsOffsetApi, setPokemonsOffsetApi] = useState(NUMBER_POKEMONS);
+
+
+
+  const handleSearchPokemons = useCallback(async () => {
+    const response = await api.get(`/pokemon?limit=${NUMBER_MAX_POKEMONS_API}`);
+
+    setPokemonSearch(pokemonSearch.toLocaleLowerCase());
+    // Valida nomes dos pokémons constam no valor da variável pokemonSearch
+    const pokemonsSearch = response.data.results.filter(({ name }) =>
+      name.includes(pokemonSearch)
+    );
+    setPokemons(pokemonsSearch);
+  }, [pokemonSearch]);
+
+  // Carrega uma lista inicial de pokémons
+  const handlePokemonsListDefault = useCallback(async () => {
+    const response = await api.get('/pokemon', {
+      params: {
+        limit: NUMBER_POKEMONS,
+      },
+    });
+    setPokemons(response.data.results);
+  }, []);
+
+  const handleMorePokemons = useCallback(
+    async (offset) => {
+      const response = await api.get(`/pokemon`, {
+        params: {
+          limit: NUMBER_POKEMONS,
+          offset,
+        },
+      });
+
+      setPokemons((state) => [...state, ...response.data.results]);
+      setPokemonsOffsetApi((state) => state + NUMBER_POKEMONS);
+    },
+    [NUMBER_POKEMONS]
+  );
+
+  useEffect(() => {
+    // A busca é só feita quando a string tiver 2 ou mais caracteres
+    const isSearch = pokemonSearch.length >= 2;
+
+    if (isSearch) handleSearchPokemons();
+    else handlePokemonsListDefault();
+  }, [pokemonSearch, handlePokemonsListDefault, handleSearchPokemons]);
+
+  return (
+    <Container>
+      <Wrapper>
+        <SearchContainer>
+          <Fieldset>
+            <Legend>Buscar</Legend>
+            <Buscar
+              type="text"
+              placeholder={'Busque seu Pokemon'}
+              value={pokemonSearch}
+              onChange={(e) => setPokemonSearch(e.target.value)}
+            />
+            <LogoBuscar src={Logopesquisar} alt="logo de pesquisa" />
+          </Fieldset>
+          <ContainerMenu>
+            <LogoFavoritos src={Logofavoritos} alt="logo favoritos" />
+            <TextFavorito>Meus Favoritos</TextFavorito>
+          </ContainerMenu>
+        </SearchContainer>
+        <PokemonsContainer>
+          {pokemons.map((pokemon) => (
+            <PokemonCard key={pokemon.name} name={pokemon.name} />
+          ))}
+        </PokemonsContainer>
+        {pokemonSearch.length <= 2 && (
+          <button
+            type="button"
+            onClick={() => handleMorePokemons(pokemonsOffsetApi)}
+          >
+            CARREGAR MAIS
+          </button>
+        )}
+      </Wrapper>
+    </Container>
+  );
+}
+export default ListPokemon;
---
+++ src/page/Home/styles.js
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1024px;
+  width: 100%;
+  margin-top: 72px;
+  align-items: center;
+  gap: 72px;
+`;
+
+export const Fieldset = styled.fieldset`
+  width: 630px;
+  height: 54px;
+  border: 2px solid #ec0344;
+  border-radius: 5px;
+  display: flex;
+  padding: 0px 40px 0 30px;
+`;
+
+export const Legend = styled.legend`
+  height: 16px;
+  font-size: 18px;
+  font-weight: 500;
+  font-style: normal;
+  color: #ec0344;
+  line-height: 16px;
+  margin-left: 5px;
+`;
+
+export const LogoBuscar = styled.img`
+  width: 27px;
+  height: 27px;
+`;
+
+export const Buscar = styled.input`
+  width: 100%;
+  height: 100%;
+  background-color: #e5e5e5;
+  border: none;
+  outline: none;
+`;
+
+export const LogoFavoritos = styled.img``;
+
+export const TextFavorito = styled.p`
+  color: #ec0344;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+`;
+
+export const ContainerMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 11px;
+`;
+
+export const PaginationContainer = styled.div`
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+export const App = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const SearchContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 4rem;
+
+  @media (max-width: 500px) : {
+    width: 100%;
+    justify-content: flex-start;
+  }
+`;
+
+export const PokemonsContainer = styled.div`
+flex: 1;
+display: grid;
+width: 100%;
+grid-template-columns: repeat(5, 18%);
+gap: 20px;
+
+  @media (max-width: 500px) : {
+    width: 100%;
+    justify-content: flex-start;
+  }
+`;
