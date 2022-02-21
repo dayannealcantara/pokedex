7ec448181494ba70111d42ee4cb30f96c32a70ca
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-21 20:18:23 -0300

    feat: criacao de rota meus favoritos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../page/Home';
 import Pokemon from '../page/Pokemon';
+import Favoritos from '../page/Favoritos';
 
 function routes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="pokemon/:name" element={<Pokemon />} />
+      <Route path="meus-favoritos" element={<Favoritos />} />
     </Routes>
   );
 }
