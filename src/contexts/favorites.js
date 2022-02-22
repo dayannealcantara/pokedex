f30e4c3b18e8afb0933f0fed625432e28bba6cff
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-22 14:23:12 -0300

    feat: criação contexto controlar pokemon favoritos, função checar pokemon favoritando

---
+++ src/contexts/favorites.js
@@ -0,0 +1,58 @@
+import { createContext, useState, useCallback, useEffect } from 'react';
+
+export const FavoritesContext = createContext([]);
+
+export function FavoritesProvider({ children }) {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const pokemonsFromLocalStorage = localStorage.getItem('pokemons')
+
+    setPokemons(JSON.parse(pokemonsFromLocalStorage) ?? []);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pokemons', JSON.stringify(pokemons))
+  }, [pokemons]);
+
+  const removePokemon = useCallback((index, pokemon) => {
+    const copy = [...pokemons]
+
+    copy.splice(index, 1);
+
+    setPokemons(copy);
+  }, [pokemons]);
+
+  const addPokemon = useCallback((pokemon) => {
+    const copy = [...pokemons]
+
+    copy.push(pokemon);
+
+    setPokemons(copy);
+  }, [pokemons])
+
+  const togglePokemon = useCallback((pokemon) => {
+    const pokemonIndex = pokemons?.findIndex((each) => each.id === pokemon.id)
+
+    if (pokemonIndex !== -1) {
+      return removePokemon(pokemonIndex, pokemon);
+    }
+
+    return addPokemon(pokemon);
+  }, [pokemons, addPokemon, removePokemon]);
+
+  const isMarked = useCallback((pokemon) => {
+    return pokemons?.findIndex((each) => each.id === pokemon.id) !== -1;
+  },[pokemons])
+
+  return (
+    <FavoritesContext.Provider value={{
+      pokemons,
+      setPokemons,
+      togglePokemon,
+      isMarked
+    }}>
+      {children}
+    </FavoritesContext.Provider>
+  )
+}
