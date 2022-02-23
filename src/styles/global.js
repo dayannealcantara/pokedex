d032e1dd9a34cbc71d00bf386f8f3216924cda81
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-23 10:05:12 -0300

    remove imprtação de font

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Arvo&family=Poppins:wght@700&display=swap');
-
-
   * {
     margin: 0;
     padding: 0;
